cd7e06d508b3b3d371f4829a9b79019d638da23a
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2024-04-15 10:26:58 +0100

    deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import ParticipantDetails from "./ParticipantDetails";
+import Login from "./Login";
+import Cookies from "js-cookie";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// HOC to check if the user is authenticated
+const ProtectedRoute = ({ element: Element, ...rest }) => {
+  const isLoggedIn = !!Cookies.get("jwt_token"); // Check if JWT token exists
+
+  // If user is not logged in, redirect to login page
+  if (!isLoggedIn) {
+    return <Navigate to="/" />;
+  }
+
+  // If user is logged in, render the provided element
+  return <Element {...rest} />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          {/* Use ProtectedRoute for protected routes */}
+          <Route path="/" element={<Login />} />
+          <Route path="/records/:id" element={<ProtectedRoute element={ParticipantDetails} />} />
+        </Routes>
+      </Router>
   );
 }
 
